af751608463ad231a22ff8bb4910307fe99eefcb
dayofpay <vladigames03@gmail.com>
2022-09-27 01:30:57 +0300

    Update Excercise.js

--- Excercise.js
+++ Excercise.js
@@ -1,17 +1,22 @@
-function printStrings(){
-    let array = [];
-    let combined = String;
-    const totalStrings = {
-        totalRows : 5,
-        character : "*",
-        startPoint : 0
+function getValues(){
+    const values = {
+        totalTries : 0,
+        books : ["In Search of Lost Time","Ulysses","Don Quixote","The Great Gatsby","Moby Dick"],
+        authors : ["Marcel Proust","James Joyce","Miguel de Cervantes","F. Scott Fitzgerald","Herman Melville"],
+        finalResult : "In Search of Lost Time"
     }
-    array.forEach(element => {
-        combined += element;
-    });
-    let calcIndex = totalStrings.totalRows * Math.PI;
-        for(let startMulti = 0; startMulti <= calcIndex; startMulti++){ // Безкраен LOOP
-        console.log(startMulti);
+    getBookData(["In Search of Lost Time","Ulysses","Don Quixote","The Great Gatsby","Moby Dick"]);
+    function getBookData(inputData){
+        let getAuthorPos = values.authors.indexOf(values.finalResult);
+        for(let startPoint = 0; startPoint <= inputData.length; startPoint++){
+            if(inputData[startPoint] != values.finalResult){
+                values.totalTries++;
+            }
+            else{
+                
+                console.log(`You found the right book after ${values.totalTries} tries \r\n Book Author is ${values.authors[values.books.indexOf(values.finalResult)]}`);
+            }
+        }
     }
 }
-printStrings();
+getValues();
