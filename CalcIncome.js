4d7efe1833c66cd5ee9dfe9689a05171c8450acb
dayofpay <vladigames03@gmail.com>
2022-09-28 03:13:25 +0300

    Create CalcIncome.js

---
+++ CalcIncome.js
@@ -0,0 +1,16 @@
+function Calculate(carPrice,mySalary,duration){
+    const data = {
+        salary : mySalary,
+        durationToGet : duration,
+        vehiclePrice : carPrice
+    }
+    let calculation = data.vehiclePrice / data.durationToGet;
+    let endMonth = data.salary - calculation;
+    if(data.salary < calculation){
+        console.log(`Не можете да си позволите тази кола, тъй като ще са ви нужни ${calculation} лева месечно, а вашата заплата е ${mySalary} ЛВ `);
+    }else{
+        console.log("Ще трябва да отделяте " + calculation + " Лева на месец за да можете да си позволите тази кола");
+        console.log("След заплащането ще ви остават " + endMonth + "лв от заделените средства");
+    }
+}
+Calculate(150000,2500,12);
