8dc34c5516007888b2beba0e0071aba7043a27ba
dayofpay <vladigames03@gmail.com>
2022-10-05 05:11:06 +0300

    Поправка на бъг

--- Holiday.js
+++ Holiday.js
@@ -1,46 +1,52 @@
 function start(input){
     let budget = Number(input.shift());
     let season = input.shift();
-    let summerPrice;
-    let winterPrice;
-    let destination;
-    let place;
-    if(budget<= 100 && season == "summer"){
+    let summerPrice = 0;
+    let winterPrice = 0;
+    let destination = "";
+    let place = "";
+    if(budget <= 100 && season == "summer"){
         summerPrice = (budget / 100) * 30;
         destination = "Bulgaria";
         place = "Camp";
-        console.log(`Somewhere in ${destination}\r\n${place} - ${summerPrice.toFixed(2)}`);
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${summerPrice.toFixed(2)}`);
     }
     else if(budget <=100 && season == "winter"){
         winterPrice = (budget / 100) * 70;
         destination = "Bulgaria";
         place = "Hotel";
-        console.log(`Somewhere in ${destination}\r\n${place} - ${winterPrice.toFixed(2)}`);
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${winterPrice.toFixed(2)}`);
     }
     if(budget <= 1000 && season == "summer" && budget >100){
         summerPrice = (budget / 100) * 40;
         destination = "Balkans";
         place = "Camp"
-        console.log(`Somewhere in ${destination}\r\n${place} - ${summerPrice.toFixed(2)}`);
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${summerPrice.toFixed(2)}`);
     }
     else if(budget <= 1000 && season == "winter" && budget > 100){
         winterPrice = (budget / 100) * 80;
         destination = "Balkans";
         place = "Hotel";
-        console.log(`Somewhere in ${destination}\r\n${place} - ${winterPrice.toFixed(2)}`);
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${winterPrice.toFixed(2)}`);
     }
     else if(budget > 1000 && season == "winter"){
-        winterPrice (budget / 100) * 90;
-        destination = "Europe";
-        place = "Hotel";
-        console.log(`Somewhere in ${destination}\r\n${place} - ${winterPrice.toFixed(2)}`);
-
-    }
-    else if(budget > 1000 && season == "summer"){
         winterPrice = (budget / 100) * 90;
         destination = "Europe";
         place = "Hotel";
-        console.log(`Somewhere in ${destination}\r\n${place} - ${winterPrice.toFixed(2)}`);
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${winterPrice.toFixed(2)}`);
+
+    }
+    else if(budget > 1000 && season == "summer"){
+        summerPrice = (budget / 100) * 90;
+        destination = "Europe";
+        place = "Hotel";
+        console.log(`Somewhere in ${destination}`);
+        console.log(`${place} - ${summerPrice.toFixed(2)}`);
 
     }
 
