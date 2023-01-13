b6e664a7d75b09eab50f73739235f4083c8ad391
dayofpay <vladigames03@gmail.com>
2023-01-13 21:54:05 +0200

    Update Spice.js

--- 2023/Data Types and Variables - Exercise/Spice.js
+++ 2023/Data Types and Variables - Exercise/Spice.js
@@ -2,18 +2,26 @@ function init(yield) {
     let days = 0;
     let consumedSpices=0;
     let total = 0;
+    let abandoned = false;
     while (yield >= 100) {
-        if(yield - 26 < 26){
-
+        if(yield > 26){
+            consumedSpices+=yield;
         }
         else{
-            consumedSpices+=yield;
+            consumedSpices+=0;
         }
         yield-=10;
         days++;
     }
-    consumedSpices-=26;
-    total = consumedSpices - (days * 26);
+    if(yield < 100){
+        abandoned = true;
+    }
+
+
+    if(abandoned){
+        consumedSpices-=26;
+    }
+    total = consumedSpices - days * 26;
 
     console.log(days);
     console.log(total);
