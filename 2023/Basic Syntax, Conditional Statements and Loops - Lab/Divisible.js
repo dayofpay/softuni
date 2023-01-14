f902762a458a124f1eb0f1028e7a079871476cc8
dayofpay <vladigames03@gmail.com>
2023-01-14 20:24:50 +0200

    New tasks

---
+++ 2023/Basic Syntax, Conditional Statements and Loops - Lab/Divisible.js
@@ -0,0 +1,8 @@
+function check(){
+    for(let x = 1; x<=100;x++){
+        if(x % 3 === 0){
+            console.log(x);
+        }
+    }
+}
+check();
---
+++ 2023/Basic Syntax, Conditional Statements and Loops - Lab/NumbersOneToFive.js
@@ -0,0 +1,5 @@
+function start(){
+    for (let index = 1; index <= 5; index++) {
+        console.log(index);
+    }
+}
