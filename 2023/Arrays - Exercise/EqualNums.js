dcb650dd8dec054ac997d1a350b4c1eba042005f
dayofpay <vladigames03@gmail.com>
2023-01-26 23:11:22 +0200

    New task

---
+++ 2023/Arrays - Exercise/EqualNums.js
@@ -0,0 +1,18 @@
+function init(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+        }
+        for (let k = i + 1; k < arr.length; k++) {
+            rightSum += arr[k];
+        }
+        if (leftSum === rightSum) {
+            console.log(i);
+            return;
+        }
+    }
+    console.log("no");
+}
+init([1,2,3,3]);
