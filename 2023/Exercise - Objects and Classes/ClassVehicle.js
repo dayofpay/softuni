70fce576378a55f56cd6deb3bcf0c04eba79419d
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-03-08 09:55:40 +0200

    new task

---
+++ 2023/Exercise - Objects and Classes/ClassVehicle.js
@@ -0,0 +1,20 @@
+class Vehicle{
+    constructor (type,model,parts,fuel){
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        }
+        this.fuel =  Number(fuel)
+    }
+    drive(fuelLoss){
+        this.fuel -= fuelLoss;
+    }
+}
+    // let parts = {engine: 6,power:100};
+    // let vehicle = new Vehicle('a','b',parts,200);
+    // vehicle.drive(100);
+    // console.log(vehicle.fuel);
+    // console.log(vehicle.parts.quality);
