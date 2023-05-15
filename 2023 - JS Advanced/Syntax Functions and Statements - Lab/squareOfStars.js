508bd6875c393534fe08978889bf22f268585d8c
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-15 21:09:34 +0300

    new task

---
+++ 2023 - JS Advanced/Syntax Functions and Statements - Lab/squareOfStars.js
@@ -0,0 +1,20 @@
+function start(size) {
+    const line = "* ".repeat(size).trim();
+  
+    for (let i = 0; i < size; i++) {
+      console.log(line);
+   
+    }
+
+    // let one = "";
+    // for(let x = 1;x<=size;x++){
+    //     one+="* ";
+    // }
+    // // while(one.includes(" ")){
+    // //     one = one.replace(' ','');
+    // // }
+    // for(let b = 1; b<= size;b++){
+    //     console.log(one);
+    // }
+  }
+start(7);
