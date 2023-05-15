24c893813aa7c6041d80986798f1e5830ed9fa16
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-15 19:44:52 +0300

    Улеснен начин за решение

--- 2023 - JS Advanced/Syntax Functions and Statements - Lab/circleArea.js
+++ 2023 - JS Advanced/Syntax Functions and Statements - Lab/circleArea.js
@@ -1,10 +1,14 @@
 function start(number){
-    if(typeof number !== "number"){
-        console.log(`We can not calculate the circle area, because we receive a ${typeof number}.`);
-    }
-    else{
-        let result = Math.pow(number,2) * Math.PI;
-        console.log(result.toFixed(2));
-    }
+    console.log(typeof number !== "number" ? `We can not calculate the circle area, because we receive a ${typeof number}.` : (Math.pow(number,2) * Math.PI).toFixed(2));
+
+    // Или ако използваме if
+
+    // if(typeof number !== "number"){
+    //     console.log(`We can not calculate the circle area, because we receive a ${typeof number}.`);
+    // }
+    // else{
+    //     let result = Math.pow(number,2) * Math.PI;
+    //     console.log(result.toFixed(2));
+    // }
 }
-start('name')
+start(5)
