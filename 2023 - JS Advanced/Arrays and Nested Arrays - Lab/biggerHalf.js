2c2d57d3bd8fb0eae0e1534ea8d907e2239cd4c2
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-19 00:07:36 +0300

    new task

--- 2023 - JS Advanced/Arrays and Nested Arrays - Lab/biggerHalf.js
+++ 2023 - JS Advanced/Arrays and Nested Arrays - Lab/biggerHalf.js
@@ -1,14 +1,22 @@
 function start(numberData){
+
+    // Sort Array
     let sortedArray = numberData.sort((a,b) => a-b);
+
+    // First Half ( Bigger )
     let firstHalf = []
+    // Second half ( Smaller )
     let secondHalf = [];
+    // Get smaller half
     for(let x = Math.floor(sortedArray.length /2); x<sortedArray.length;x++){
         secondHalf.push(sortedArray[x]);
     }
+    // Get bigger half
     for(let b = 0;b<Math.floor(sortedArray.length/2);b++){
         firstHalf.push(sortedArray[b]);
     }
 
+    // Return the result :^)
     return sortedArray % 2 ===0 ? firstHalf : secondHalf;
 }
 console.log(start([3, 19, 14, 7, 2, 19, 6]));
