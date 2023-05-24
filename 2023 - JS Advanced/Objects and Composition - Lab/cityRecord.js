51c294710ade7d3281a2aabe5c2e1a199b65da96
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-24 22:06:57 +0300

    new task

---
+++ 2023 - JS Advanced/Objects and Composition - Lab/cityRecord.js
@@ -0,0 +1,12 @@
+function start(inputName,inputPopulation,inputTreasury){
+    let data = {
+        name : inputName,
+        population : inputPopulation,
+        treasury : inputTreasury,
+    }
+
+    return data;
+}
+start('Tortuga',
+7000,
+15000);
