3ac48028dc84ca4fc3c3dd51205907ef73789c7a
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-24 22:56:14 +0300

    new task

---
+++ 2023 - JS Advanced/Objects and Composition - Lab/objectFactory.js
@@ -0,0 +1,56 @@
+function factory(library, orders) {
+    const products = [];
+  
+    for (const order of orders) {
+      const product = Object.assign({}, order.template);
+  
+      for (const part of order.parts) {
+        if (library[part]) {
+          product[part] = library[part].bind(product);
+        }
+      }
+  
+      products.push(product);
+    }
+  
+    return products;
+  }
+  
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+const orders = [{
+        template: {
+            name: 'ACME Printer'
+        },
+        parts: ['print']
+    },
+    {
+        template: {
+            name: 'Initech Scanner'
+        },
+        parts: ['scan']
+    },
+    {
+        template: {
+            name: 'ComTron Copier'
+        },
+        parts: ['scan', 'print']
+    },
+    {
+        template: {
+            name: 'BoomBox Stereo'
+        },
+        parts: ['play']
+    }
+];
+const products = factory(library, orders);
+console.log(products);
