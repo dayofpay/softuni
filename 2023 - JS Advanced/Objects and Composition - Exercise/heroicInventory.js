3135c47a8455ff1867ae30af739c136ee9159e75
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-05-29 04:44:41 +0300

    new task

--- 2023 - JS Advanced/Objects and Composition - Exercise/heroicInventory.js
+++ 2023 - JS Advanced/Objects and Composition - Exercise/heroicInventory.js
@@ -1,1 +1,13 @@
-// todo
+function start(inputData){
+    let heroes = [];
+    for(let hero of inputData){
+        let [name,level,items] = hero.split(" / ");
+        level = Number(level);
+        items = items ? items.split(', ') : [];
+        heroes.push({name,level,items});
+    }
+    console.log(JSON.stringify(heroes));
+}
+start(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']);
