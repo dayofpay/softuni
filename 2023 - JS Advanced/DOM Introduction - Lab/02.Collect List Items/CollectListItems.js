927f7ceeffae5971ceb9710358591b993ab77f77
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-01 11:17:10 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ 2023 - JS Advanced/DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let getElements = document.getElementsByTagName('li');
+    let elementsToArray = Array.from(getElements);
+
+    let allTexts = elementsToArray.map(x => x.textContent);
+
+    let textArea = document.getElementById('result').value = allTexts
 }
