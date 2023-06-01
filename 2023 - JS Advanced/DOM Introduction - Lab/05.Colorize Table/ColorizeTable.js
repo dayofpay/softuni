84321c6958b8cf8d35fee22bd2c6cb203bdf923b
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-01 11:41:51 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ 2023 - JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,6 @@
 function colorize() {
-    // TODO
+    let getRows = document.getElementsByTagName('tr');
+    for(let x = 0; x < getRows.length;x++){
+        x % 2 !== 0 ? getRows[x].style.backgroundColor="teal" : null;
+    } 
 }
