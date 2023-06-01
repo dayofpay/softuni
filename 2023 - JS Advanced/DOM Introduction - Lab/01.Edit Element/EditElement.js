fd16d4e515af8debb0a48f0269ed509bd26df4ec
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-01 10:20:34 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ 2023 - JS Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,4 +1,6 @@
 function editElement(ref,match,replaceText) {
     let getText = ref.textContent;
-    console.log(getText);
+    let regex = new RegExp(match,'g')
+    let res = getText.replace(regex,replaceText);
+    ref.textContent = res;
 }
