5a48fc0765f9e33a55f048c81f30c3a9bdc34281
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 21:42:18 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ 2023 - JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let getAllRows = document.getElementsByTagName('tr');
+      let searchData = document.getElementById('searchField').value;
+      for (let x = 2; x < getAllRows.length; x++) {
+         getAllRows[x].classList.remove('select');
+         if (getAllRows[x].textContent.includes(searchData) && searchData.length >= 1) {
+            //getAllRows[x].style.background = "yellow";
+            getAllRows[x].classList.add('select')
+         }
+      }
 
    }
 }
