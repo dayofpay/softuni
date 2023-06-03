b5a84c1f2aa94f8c926f4b82ff46db25e5ea2435
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 16:23:40 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ 2023 - JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   let getElements = document.getElementsByTagName('li');
+   let cities = Array.from(getElements);
+   let getSearchData = document.getElementById('searchText').value.toLowerCase();
+   let totalMatches = 0;
+
+   cities.map(city => {
+      city.style.fontWeight = "normal";
+      city.style.textDecoration = "none";
+      if (city.innerText.toLowerCase().includes(getSearchData)) {
+         city.style.fontWeight = "bold";
+         city.style.textDecoration = "underline";
+         totalMatches++;
+      }
+   });
+
+   document.getElementById('result').innerText = totalMatches + " matches found";
 }
