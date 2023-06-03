3c7e256349ec85464093f06a1dad891e76a01ea8
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 12:20:16 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 2023 - JS Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,24 @@
 function extract(content) {
-
-}
+    let getContent = document.getElementById(content);
+    let result = [];
+    let test = getContent.innerHTML;
+    let start = 0;
+    
+    while (start < test.length) {
+      if (test[start] === "(") {
+        let end = test.indexOf(")", start + 1); 
+        if (end !== -1) {
+          let extractedText = test.substring(start + 1, end);
+          result.push(extractedText.trim());
+          start = end + 1;
+        } else {
+          break;
+        }
+      } else {
+        start++;
+      }
+    }
+    
+    return result.join("; ");
+  }
+  
