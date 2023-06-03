8538169aa444c1f79ba93ac15eabac9cb9488066
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 15:40:45 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 2023 - JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let extraElement = document.getElementById('extra');
+    let buttonElement = document.getElementsByClassName('button')[0];
+    if (extraElement.style.display === "none") {
+        extraElement.style.display = "block";
+        buttonElement.textContent = "Less";
+    } else {
+        extraElement.style.display = "none";
+        buttonElement.textContent = "More";
+    }
 }
