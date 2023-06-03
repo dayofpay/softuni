2ba4c24f8735afc3900e337276d494504e9c10e0
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 22:32:23 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ 2023 - JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+  let inputData = document.getElementById('input').value;
+  let sentences = inputData.split('.');
+  let paragraphs = [];
+
+  while (sentences.length > 0) {
+    let paragraph = sentences.splice(0, 3).join('. ');
+    paragraphs.push(paragraph);
+  }
+
+  const outputElement = document.getElementById('output');
+  outputElement.innerHTML = '';
+
+  for (let paragraph of paragraphs) {
+    const createParagraph = document.createElement('p');
+    createParagraph.innerHTML = paragraph.trim();
+    outputElement.appendChild(createParagraph);
+  }
 }
