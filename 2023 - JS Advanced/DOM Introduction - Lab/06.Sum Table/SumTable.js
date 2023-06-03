a0c85e7e87a6897aeeb8097a1de1449ff8de493c
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-03 12:04:41 +0300

    new solution

--- 2023 - JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ 2023 - JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    let getElements = document.getElementsByTagName('td');
+    let getSum = document.getElementById('sum');
+    let totalSum = 0;
+    for(let x = 1; x < getElements.length - 2;x+=2){
+        totalSum += Number(getElements[x].innerHTML);
+    }
+    getSum.textContent = totalSum;
 }
