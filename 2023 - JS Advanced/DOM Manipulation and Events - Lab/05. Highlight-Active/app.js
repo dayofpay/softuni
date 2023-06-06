20f606dac0ef085fad1440f252b079175cdfb87a
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-06 19:20:15 +0300

    new solution

--- 2023 - JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let divElements = document.getElementsByTagName('div');
+
+    for (let i = 0; i < divElements.length; i++) {
+        let inputElement = divElements[i].querySelector('input');
+        inputElement.addEventListener('focus', focusIn);
+        inputElement.addEventListener('blur', focusOut);
+    }
+
+    function focusIn(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function focusOut(event) {
+        event.target.parentElement.classList.remove('focused');
+    }
 }
