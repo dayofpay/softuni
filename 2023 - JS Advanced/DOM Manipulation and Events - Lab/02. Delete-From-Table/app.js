ed0f1991cf4666e9776c49d3a2676dc76ca6c7d3
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-06 17:07:12 +0300

    new solution

--- 2023 - JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let getEmail = document.getElementsByName('email')[0].value;
+
+    let resultData = document.getElementById('result');
+
+    let getAllRows = document.getElementsByTagName('tr');
+
+    for(let x = 0; x < getAllRows.length; x++){
+        if(getAllRows[x].textContent.includes(getEmail)){
+            getAllRows[x].remove();
+            resultData.innerText = "Deleted.";
+        }
+        else{
+            resultData.innerText = "Not found.";
+
+        }
+    }
 }
