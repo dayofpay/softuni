62f14fa103c73ff1af91c9ea39a3f8e07e9b758d
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-06 23:25:04 +0300

    new solution

--- 2023 - JS Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInput = document.getElementById('email');
+
+    emailInput.addEventListener('change', typeEvent);
+
+    function typeEvent(e) {
+        let getEmail = e.target.value;
+
+        if (!getEmail.includes('@')) {
+            emailInput.classList.add('error');
+        } else {
+            emailInput.classList.remove('error');
+        }
+    }
 }
