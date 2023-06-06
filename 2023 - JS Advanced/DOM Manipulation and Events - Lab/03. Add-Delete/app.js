261f2e0ccc4cafc89d704feea429e3902f1b0cf2
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-06 17:26:19 +0300

    new solution

--- 2023 - JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let getItemName = document.getElementById('newItemText').value;
+    let getItemsList = document.getElementById('items');
+
+    let newItem = document.createElement('li');
+    newItem.innerText = getItemName;
+
+    let createDeleteButton = document.createElement('a');
+    createDeleteButton.textContent = "[Delete]";
+    createDeleteButton.href = "#";
+    createDeleteButton.addEventListener('click', deleteItem);
+
+    newItem.appendChild(createDeleteButton);
+    getItemsList.appendChild(newItem);
+
+    function deleteItem(e) {
+        let target = e.target;
+        let item = target.parentElement;
+        item.remove();
+    }
 }
