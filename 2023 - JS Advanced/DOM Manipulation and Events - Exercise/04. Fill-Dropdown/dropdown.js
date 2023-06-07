d306d17e9d1c91c0db8c07a222fb636056a1a56e
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-07 20:44:57 +0300

    new task

--- 2023 - JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
-}
+
+    let newItemText = document.getElementById('newItemText').value;
+    let newItemValue = document.getElementById('newItemValue').value;
+  
+
+    let option = document.createElement('option');
+    option.textContent = newItemText;
+    option.value = newItemValue;
+  
+
+    let menu = document.getElementById('menu');
+    menu.appendChild(option);
+  
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+  }
+  
