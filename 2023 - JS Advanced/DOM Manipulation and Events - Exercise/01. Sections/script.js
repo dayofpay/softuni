2a95884af0cc30b10ccec9bb5c266d8455a2ddcb
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-07 19:58:17 +0300

    new task

--- 2023 - JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ 2023 - JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,30 @@
 function create(words) {
-   console.log('TODO:...');
+   let createElement = document.getElementById('content');
+
+
+   for (let word of words) {
+      let words = createElement.appendChild(document.createElement('div'));
+      let createParagraph = document.createElement('p');
+      let elementText = document.createTextNode(word);
+
+      createParagraph.appendChild(elementText);
+
+      words.appendChild(createParagraph);
+
+      createElement.addEventListener('click', revealWord);
+
+      createParagraph.style.display = "none";
+
+   }
+
+
+   document.body.appendChild(createElement);
+
+   function revealWord(e) {
+      let clickedDiv = e.target;
+      let paragraph = clickedDiv.querySelector('p');
+
+      paragraph.style.display = "inline";
+   }
+
 }
