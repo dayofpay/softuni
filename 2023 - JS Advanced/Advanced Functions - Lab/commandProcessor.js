294cb39c9f7e0c043fd14e8e7998dc6460c683ab
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-13 00:26:36 +0300

    modify

--- 2023 - JS Advanced/Advanced Functions - Lab/commandProcessor.js
+++ 2023 - JS Advanced/Advanced Functions - Lab/commandProcessor.js
@@ -17,12 +17,12 @@
 
 
 function result() {
-    let collection = '';
-  
-    return {
-      append: (text) => (collection += text),
-      removeStart: (n) => (collection = collection.slice(n)),
-      removeEnd: (n) => (collection = collection.slice(0, -n)),
-      print: () => console.log(collection)
-    };
-  }
+  let collection = '';
+
+  return {
+    append: (text) => (collection += text),
+    removeStart: (n) => (collection = collection.slice(n)),
+    removeEnd: (n) => (collection = collection.slice(0, -n)),
+    print: () => console.log(collection)
+  };
+}
