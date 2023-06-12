4419d87654a14825d523d7cbf62cee4ac68b304a
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-12 21:50:19 +0300

    bug fix

--- 2023 - JS Advanced/Advanced Functions - Lab/add.js
+++ 2023 - JS Advanced/Advanced Functions - Lab/add.js
@@ -2,13 +2,14 @@ function solution(number) {
     let currentNumber = number;
   
     function addFunction(value) {
-      currentNumber += value;
-      return currentNumber;
+      return currentNumber + value;
     }
   
     return addFunction;
   }
   
-  let add5 = solution(5);
-  console.log(add5(2)); // Output: 7 (5 + 2)
-  console.log(add5(3)); // Output: 10 (7 + 3)
+
+let add7 = solution(7);
+console.log(add7(2)); 
+console.log(add7(3)); 
+  
