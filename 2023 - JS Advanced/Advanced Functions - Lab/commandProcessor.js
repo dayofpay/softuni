800b7ee7b5ca2351b60961184c1d84bdb8bc6511
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-13 00:29:37 +0300

    Modiy & new solutions

--- 2023 - JS Advanced/Advanced Functions - Lab/commandProcessor.js
+++ 2023 - JS Advanced/Advanced Functions - Lab/commandProcessor.js
@@ -1,21 +1,3 @@
-// function result(commands) {
-//     let collection = [];
-
-//     const executeCommand = {
-//       add: (item) => collection.push(item),
-//       remove: (item) => collection = collection.filter(x => x !== item),
-//       print: () => console.log(collection.join(','))
-//     };
-  
-//     Array.from(commands).forEach(command => {
-//       let [action, value] = command.split(' ');
-//       executeCommand[action](value);
-//     });
-//   }
-//   result(['add pesho', 'add george', 'add peter', 'remove peter', 'print']);
-  
-
-
 function result() {
   let collection = '';
 
