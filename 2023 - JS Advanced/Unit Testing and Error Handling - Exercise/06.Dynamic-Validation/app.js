e5e77ee6a5162ed28ac31d27c2bb2ca2a9cca67c
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-20 22:23:28 +0300

    new solution

--- 2023 - JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ 2023 - JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
-}
+    const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+    const getEmailInput = document.getElementById('email');
+  
+    getEmailInput.addEventListener('change', emailEvent);
+  
+    function emailEvent(e) {
+      if (emailRegex.test(getEmailInput.value)) {
+        getEmailInput.classList.remove('error');
+      } else {
+        getEmailInput.classList.add('error');
+      }
+    }
+  }
+  
