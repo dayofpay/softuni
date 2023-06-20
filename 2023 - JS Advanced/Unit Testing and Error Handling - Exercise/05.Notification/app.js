c4096cd711a9f7d47313743e15184e06d6816a60
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-20 21:17:05 +0300

    new task

--- 2023 - JS Advanced/Unit Testing and Error Handling - Exercise/05.Notification/app.js
+++ 2023 - JS Advanced/Unit Testing and Error Handling - Exercise/05.Notification/app.js
@@ -1,4 +1,12 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+
+  let getNotificationElement = document.getElementById('notification');
+  getNotificationElement.style.display = "block";
+  getNotificationElement.textContent = message;
+
+  getNotificationElement.addEventListener('click',eListener);
+
+  function eListener(e){
+    getNotificationElement.style.display = "none";
+  }
 }
