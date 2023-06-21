aaf9c4436bdacdb338517b83f087d462ab5ab2d0
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-22 01:47:28 +0300

    new solution

---
+++ 2023 - JS Advanced/Prototypes and Inheritance - Lab/extendPrototype.js
@@ -0,0 +1,6 @@
+function extendPrototypes(Class){
+    Class.prototype.species = "Human";
+    Class.prototype.toSpeciesString = function(){
+        return `I am a ${this.species}. ${this.toString()}`
+    }
+}
