a0c72f369e093d05a07b39626b1fa6da247e3361
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-06-30 17:53:34 +0300

    part 1 - implemented the login view and others

--- 2023 - JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ 2023 - JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -1,1 +1,26 @@
-console.log('TODO:// Implement Home functionality');
+// check if user is logged
+let navEmail = document.getElementsByClassName('email')[0];
+    try {
+     if(sessionStorage.getItem('accessToken') !== null){
+        // change user navigation
+
+
+
+        navEmail.innerHTML = `
+        Welcome, <span>${sessionStorage.getItem('auth_user')}</span></p>
+        `
+     }
+     else{
+        navEmail.innerHTML = `
+        Welcome, <span>guest</span></p>
+        `
+     }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle error and display appropriate error message
+    }
+  
+
+    function unAuthorizedActions(){
+        console.log(true);
+    }
--- 2023 - JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/login.js
+++ 2023 - JS Applications/Data and Authentication - Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,54 @@
-console.log('TODO:// Implement Login functionality');
+let loginEndpoint = `http://localhost:3030/users/login`;
+
+
+let getSubmitButton = document.getElementById('loginButton');
+
+getSubmitButton.addEventListener('click',authUser);
+
+let getNotificationBar = document.getElementsByClassName('notification')[0];
+
+async function authUser(event){
+    // Remove Access Token
+
+    sessionStorage.removeItem('accessToken');
+    let getUser = document.getElementsByName('email')[0].value;
+    let getPassword = document.getElementsByName('password')[0].value;
+    event.preventDefault();
+    let postData = {
+        email : getUser,
+        password : getPassword
+    }
+    let loginSettings = await fetch(loginEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      });
+    
+      if (loginSettings.status === 403) {
+        // Unsuccessful authentiaction
+
+        getNotificationBar.style.color = "red";
+
+        getNotificationBar.textContent = `Wrong username OR password, please try again !`;
+
+
+      }
+      else{
+        // Success Auth
+        let response = await loginSettings.json();
+        sessionStorage.setItem('accessToken',await response["accessToken"]);
+
+        sessionStorage.setItem('auth_user',await response["username"]);
+
+        sessionStorage.setItem(`auth_email`,await response["email"]);
+
+        getNotificationBar.style.color = "green";
+
+        getNotificationBar.textContent = `Welcome, ${sessionStorage.getItem('auth_user')} !`;
+
+      }
+      console.log(sessionStorage.getItem('accessToken'));
+}
+
