fd8101396a979367d42f98125c5ce1ae821bc9a1
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-10-15 06:13:44 +0300

    Final - Implemented search logic

--- 2023 - JS BackEnd/Workshop MongoDB and Mongoose/controllers/homeController.js
+++ 2023 - JS BackEnd/Workshop MongoDB and Mongoose/controllers/homeController.js
@@ -1,25 +1,44 @@
 const router = require("express").Router();
 
 const cubeServices = require('../services/cubeServices');
+const Cube = require('../models/Cubes');
 const aboutData = require('../database/methods')
 router.get('/', async (req, res) => {
-  try {
+
+  const {
+    search,
+    from,
+    to
+  } = req.query;
+  if (!search && !from && !to) {
+    try {
       const cubeData = await cubeServices.listCubes();
       const mapped = JSON.stringify(cubeData);
-      res.render('index',{cubeData: JSON.parse(mapped)})
-  } catch (error) {
+      res.render('index', {
+        cubeData: JSON.parse(mapped)
+      })
+    } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({
+        error: 'Internal Server Error'
+      });
+    }
+  }else{
+    let result = await Cube.find({ name: search, diff_level: { $gte: from,$lte: to } }).exec();
+    const mapped = JSON.stringify(result);
+    res.render('index',{cubeData:JSON.parse(mapped)})
   }
 });
 
 router.get("/about", async (req, res) => {
   let data = await aboutData.getAbout();
   const mapped = JSON.stringify(data);
-  res.render("about",{aboutData: JSON.parse(mapped)});
+  res.render("about", {
+    aboutData: JSON.parse(mapped)
+  });
 });
 
-router.get('/create',(req,res) => {
-    res.render('create');
+router.get('/create', (req, res) => {
+  res.render('create');
 })
 module.exports = router;
