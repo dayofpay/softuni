484dd7928c81f7957d0591600f2eb568fcac045b
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-10-18 12:13:10 +0300

    Verification

--- 2023 - JS BackEnd/Session and Authentication/JWT.js
+++ 2023 - JS BackEnd/Session and Authentication/JWT.js
@@ -20,7 +20,7 @@ async function generateHash(password) {
 
 app.get('/', async (req, res) => {
     try {
-        // Generate hashed password
+        // Generate hashed password ( Just for the demo, JWT Demonstration is our main priority here )
         const hashedPassword = await generateHash('parola123');
 
         const jwtConfig = {
@@ -38,16 +38,23 @@ app.get('/', async (req, res) => {
         console.log(true);
         const jwt_sign = jwt.sign(jwtConfig.payLoadData, jwtConfig.secret.token, { expiresIn: '30d' });
 
-        // Verification
-        const verify = jwt.verify(jwt_sign, jwtConfig.secret.token, function(err, decoded) {
-            return err !== null ? { JWT_EXCEPTION: 'Invalid Signature' } : decoded;
-        });
 
-        console.log(verify);
-        res.send(verify);
+        res.send(jwt_sign)
     } catch (error) {
         res.status(500).send('Error generating token: ' + error.message);
     }
+});
+
+
+// Demonstration with verifying JWT using request
+
+app.get('/verify/:token', async (req, res) => {
+        // Verification
+        const verify = jwt.verify(req.params.token, 'SecretToken7777', function(err, decoded) {
+            return err !== null ? { JWT_EXCEPTION: 'Invalid Signature' } : decoded;
+        });
+        console.log(true);
+        res.send(verify)
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT} ...`));
