88fe1db3aae89004343aeafdd4176bb9d574b793
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-02 04:12:53 +0200

    Demo with multiple timers using own state

--- 2023 - ReactJS/Components - Basic Idea/components-basics/src/App.jsx
+++ 2023 - ReactJS/Components - Basic Idea/components-basics/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
      <div>
       <Heading>Dynamic React Application</Heading>
       <Timer startTime={5} endTime={10}/>
+      <Timer startTime={15} endTime={20}/>
+      <Timer startTime={20} endTime={25}/>
+
       <MovieList headingText="Movie List" movieList={movies} secondaryText="Secondary Text"/>
      </div>
     </>
