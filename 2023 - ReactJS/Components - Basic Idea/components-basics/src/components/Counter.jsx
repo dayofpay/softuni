8445ea1ce1147d066b4d7948e500294b1998ed42
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-03 03:48:43 +0200

    Another test

--- 2023 - ReactJS/Components - Basic Idea/components-basics/src/components/Counter.jsx
+++ 2023 - ReactJS/Components - Basic Idea/components-basics/src/components/Counter.jsx
@@ -9,6 +9,9 @@ export default function Counter(props){
     }
 
     const decrementCounter = () => {
+        if(count-1 < 0){ // If we want to not allow users decrement value bellow 0
+            return;
+        }
         setCount(count-1);
     }
 
@@ -16,15 +19,18 @@ export default function Counter(props){
         setCount(0);
     }
 
+    let counter = count < 0 ? <p>Invalid Counter</p> : null;
+    
     return (
         <div>
             <h1>Counter</h1>
 
             <p>Count : {count}</p>
-
+            {counter}
             <button onClick={incrementCounter}>+</button>
             <button onClick={resetCounter}>Reset</button>
             <button onClick={decrementCounter}>-</button>
         </div>
     )
+    
 }
