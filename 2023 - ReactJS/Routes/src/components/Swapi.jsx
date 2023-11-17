337c5b5ba3535e3f11e2b0acafe9acef605ef153
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-17 05:38:28 +0200

    Added error handling

--- 2023 - ReactJS/Routes/src/components/Swapi.jsx
+++ 2023 - ReactJS/Routes/src/components/Swapi.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export default function Swapi(){
     let api_url = "https://swapi.dev/api"
     const [characters,setCharacters] = useState([]);
-
+    const [hasError,setError] = useState(false);
     useEffect(() => {
 
 
@@ -11,11 +11,18 @@ export default function Swapi(){
           .then(res => res.json())
           .then(data => {
              setCharacters(data.results);
+         }).catch((err) => {
+            setError(true);
+            console.log('Error',err);
          })
     },[])
 
-    while(!characters.length){
+    while(!hasError && !characters.length){
         return <div style={{color:'red', fontSize:'55px'}}>Loading, please wait ;) ...</div>
+    }
+
+    if(hasError){ // Error handling in case something happens while trying to fetch
+        return <div style={{color:'red'}}>Error while trying to fetch data from the API</div>
     }
     return(
         <ul>
